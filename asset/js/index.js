10a718f88356884c5e14931de859affcac28f215
jiahaowa <jiahao.wang@intel.com>
2017-05-10 19:53:30 +0800

    change the data source of  table

---
+++ asset/js/index.js
@@ -0,0 +1,38 @@
+var data = [
+    {
+        "task id": "bootstrap-table",
+        "version": "526",
+        "board": "122",
+        "status": "An extended Bootstrap table with radio, checkbox, sort, pagination, and other added features. (supports twitter bootstrap v2 and v3) "
+    },
+    {
+        "task id": "bootstrap-table",
+        "version": "526",
+        "board": "122",
+        "status": "An extended Bootstrap table with radio, checkbox, sort, pagination, and other added features. (supports twitter bootstrap v2 and v3) "
+    },
+    {
+        "task id": "bootstrap-table",
+        "version": "526",
+        "board": "122",
+        "status": "An extended Bootstrap table with radio, checkbox, sort, pagination, and other added features. (supports twitter bootstrap v2 and v3) "
+    },
+    {
+        "task id": "bootstrap-table",
+        "version": "526",
+        "board": "122",
+        "status": "An extended Bootstrap table with radio, checkbox, sort, pagination, and other added features. (supports twitter bootstrap v2 and v3) "
+    },
+    {
+        "task id": "bootstrap-table",
+        "version": "526",
+        "board": "122",
+        "status": "An extended Bootstrap table with radio, checkbox, sort, pagination, and other added features. (supports twitter bootstrap v2 and v3) "
+    },
+];
+
+$(function () {
+  $('#table').bootstrapTable({
+      data: data
+  });
+});
